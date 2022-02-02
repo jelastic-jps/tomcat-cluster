c74af684fe6e468ac96e391914ec64daa5662a63
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2022-02-02 15:13:32 +0200

    Update quotascheck.js

--- scripts/quotascheck.js
+++ scripts/quotascheck.js
@@ -3,7 +3,17 @@ var MAX_CLOUDLET = "environment.maxcloudletsperrec",
     MAX_NODES = "environment.maxnodescount",
     SSL = "environment.jelasticssl.enabled";
 
-var max = 10, cloudlets = 6, min = 2, resp, name, value, ssl, markup = "", q = jelastic.billing.account.GetQuotas(MAX_NODES + ";" + SAME_NODES + ";" + MAX_CLOUDLET + ";" + SSL).array || [];
+var max = 10, cloudlets = 6, min = 2, resp, name, value, ssl, markup = "";
+
+var hasCollaboration = (parseInt('${fn.compareEngine(7.0)}', 10) >= 0),
+    q = [];
+
+if (hasCollaboration) {
+    quotas = JSON.parse('${quota.data}');
+    quotas = [ quotas[MAX_CLOUDLET], quotas[SAME_NODES], quotas[MAX_NODES], quotas[SSL] ];
+} else {
+    quotas = jelastic.billing.account.GetQuotas(MAX_NODES + ";" + SAME_NODES + ";" + MAX_CLOUDLET + ";" + SSL).array || [];
+}
 
 for (var i = 0, n = q.length; i < n; i++) {
   name = q[i].quota.name;
